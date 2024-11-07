1fcf940165a72493c78f09a08fc801cd94d23870
Kanik Goyal <63117613+7999999166@users.noreply.github.com>
2024-11-07 12:20:34 +0530

    wxwx

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,9 +1,29 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+   {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::poweredBy',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: ['http://localhost:1337','http://localhost:5173','netlify app http address',' Railway app http address','capacitor://localhost','https://localhost']
+    }
+  },
   'strapi::query',
   'strapi::body',
   'strapi::session',
--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,48 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+   },
+   // ...
+  // ...
+  "users-permissions": {
+    config: {
+      register: {
+        allowedFields: [ "PhoneNo" , "Address" ],
+      },
+    },
+  },
+  // ...
+ // ...
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        service: 'gmail' ,
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+        
+      },
+      settings: {
+        defaultFrom: 'assipl023@gmail.com',
+        defaultReplyTo: 'acrotechllp@gmail.com',
+      },
+    },
+  },
+  // ...
+   //...
+
+  });
